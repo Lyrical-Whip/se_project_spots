c5ad3e07b6aee1c4af7c5dcfb3b2da572a66c178
Lyric <eraleigh8@gmail.com>
2025-11-09 14:13:59 +0800

    java

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+
+const newPostBtn = document.querySelector(".profile__new-post-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add(".modal_is-open");
+});
+
+editProfileCLoseBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove(".modal_is-open");
+});
+
+newPostBtnBtn.addEventListener("click", function () {
+  newPostModal.classList.add(".modal_is-open");
+});
+
+newPostCloseBtn.addEventListener("click", function () {
+  newPostModal.classList.remove(".modal_is-open");
+});
