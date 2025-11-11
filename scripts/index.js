842a36d377bdbc41de40622d8f120c2e3107495b
Lyric <eraleigh8@gmail.com>
2025-11-11 14:12:02 +0800

    made corrections

--- scripts/index.js
+++ scripts/index.js
@@ -7,17 +7,17 @@ const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 editProfileBtn.addEventListener("click", function () {
-  editProfileModal.classList.add(".modal_is-open");
+  editProfileModal.classList.add("modal__is-opened");
 });
 
-editProfileCLoseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove(".modal_is-open");
+editProfileCloseBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal__is-opened");
 });
 
-newPostBtnBtn.addEventListener("click", function () {
-  newPostModal.classList.add(".modal_is-open");
+newPostBtn.addEventListener("click", function () {
+  newPostModal.classList.add("modal__is-opened");
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove(".modal_is-open");
+  newPostModal.classList.remove("modal__is-opened");
 });
